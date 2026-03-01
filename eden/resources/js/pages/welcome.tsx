58ff7b81384138ec66c7d8dcd0e22cdb5d71c617
Daina Parducho <dainajoyperolina@gmail.com>
2026-03-01 22:53:27 +0800

    feat(welcome): reorder components for improved layout flow

--- eden/resources/js/pages/welcome.tsx
+++ eden/resources/js/pages/welcome.tsx
@@ -1,17 +1,18 @@
 import Impact from '@/components/eden-components/impact';
 import Hero from '@/components/eden-components/hero';
-import { Stack, Divider } from '@mui/material';
 import ProblemSection from '@/components/eden-components/problem-section';
 import SolutionSection from '@/components/eden-components/solution-section';
+import CallToAction from '@/components/eden-components/call-to-action';
 import Footer from '@/components/eden-components/footer';
 
 export default function Welcome() {
     return (
         <>
             <Hero />
-            <SolutionSection />
             <ProblemSection />
+            <SolutionSection />
             <Impact />
+            <CallToAction />
             <Footer />
         </>
     );
