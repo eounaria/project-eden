e71bba39f356390a1752159aa97a693566537b1d
Daina Parducho <dainajoyperolina@gmail.com>
2026-03-01 20:38:54 +0800

    feat(welcome): restructure Welcome component layout and remove Typography

--- eden/resources/js/pages/welcome.tsx
+++ eden/resources/js/pages/welcome.tsx
@@ -1,9 +1,18 @@
-import { Typography } from '@mui/material';
+import Impact from '@/components/eden-components/impact';
+import Hero from '@/components/eden-components/hero';
+import { Stack, Divider } from '@mui/material';
+import ProblemSection from '@/components/eden-components/problem-section';
+import SolutionSection from '@/components/eden-components/solution-section';
+import Footer from '@/components/eden-components/footer';
 
 export default function Welcome() {
     return (
-        <div className="p-2">
-            <Typography variant="h3">Welcome to Eden!</Typography>
-        </div>
+        <>
+            <Hero />
+            <SolutionSection />
+            <ProblemSection />
+            <Impact />
+            <Footer />
+        </>
     );
 }
